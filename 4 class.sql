9cbebbc59ada85056f508bc18aa39be90b766a5a
gaowanlu <2209120827@qq.com>
2021-01-21 18:06:11 +0800

    添加第4课WHERE子句

---
+++ 4 class.sql
@@ -0,0 +1,24 @@
+/*                                           第三课(过滤数据)                                                         */
+# WHERE子句
+USE sql_inventory;
+SELECT *
+FROM products
+WHERE unit_price = 1.21;
+
+# WHERE子句操作符
+/*      符号      说明
+		 =        等于
+		<>        不等于
+        !=        不等于
+        <         小于
+        <=        小于等于
+        !<        不小于
+        >         大于
+        >=        大于等于
+        !>        不大于
+        BETWEEN A AND B   在指定的A B之间
+		IS NULL   为NULL值
+*/
+
+
+
